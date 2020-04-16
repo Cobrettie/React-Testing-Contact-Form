318e44ef9c8c82107acc9702b0495f67c9383db4
Cobrettie <cobrettiegarner3@gmail.com>
2020-04-15 20:24:34 -0400

    edit readme

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,13 @@ import App from "./App";
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+it('renders without crashing', () => {
+  const wrapper = render(
+    <span>Hello World</span>
+  )
+  const element = wrapper.queryByText(/hello/i)
+  expect(element).toBeTruthy()
+  expect(element).toBeInTheDocument()
+  expect(element).toBeVisible()
+})
